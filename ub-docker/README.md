c003faa329fac24f07b5154f054e7a5c9048d475
Du Dang <hotmit@gmail.com>
2022-11-16 17:11:32 -0500

    added docker script

---
+++ ub-docker/README.md
@@ -0,0 +1,4 @@
+# Install Docker On To Ubuntu LTS
+```
+wget -q -O - https://raw.githubusercontent.com/hotmit/dotfiles/ub-docker/docker.sh | sh
+```
---
+++ ub-docker/docker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+    echo 'You must be root'
+    exit
+fi
+
+if [ ! -f /etc/ssh/sshd_config.bak ]
+    /cp -f /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+
+    echo "
+
+PermitRootLogin yes
+PasswordAuthentication yes" >> /etc/ssh/sshd_config
+
+    service sshd reload
+
+    # Ref: https://docs.docker.com/engine/install/ubuntu/
+    apt-get update -y
+    apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release -y
+
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    apt-get update -y
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+    docker run hello-world
+
+fi
+
